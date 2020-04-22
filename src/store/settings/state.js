1688f4c1b1079e9929ab9c0ace9024751021a4f2
Tucsky <kevin.rostagni@gmail.com>
2020-04-23 00:07:12 +0200

    bugfix

--- src/store/settings/state.js
+++ src/store/settings/state.js
@@ -9,10 +9,18 @@ import DEFAULTS from './defaults.json'
 const QUERY_STRING = parseQueryString()
 
 /**
- * ACTUAL STORED OBJECT
+ * ACTUAL STORED SETTINGS
  */
 
-const STORED = JSON.parse(localStorage.getItem('settings'))
+let STORED;
+
+try {
+  STORED = JSON.parse(localStorage.getItem('settings')) || {}
+} catch (error) {
+  console.log('[store] failed to load settings')
+
+  STORED = {}
+}
 
 /**
  *  EXTRA
@@ -26,14 +34,20 @@ const EXTRA = {}
 const subdomain = window.location.hostname.match(/^([\d\w\-\_]+)\..*\./i)
 const except = ['beta', 'www']
 
-if (subdomain && subdomain.length >= 2 && except.indexOf(except) === -1) {
+if (subdomain && subdomain.length >= 2 && except.indexOf(subdomain) === -1) {
   EXTRA.pair = subdomain[1].replace(/\_/g, '+').toUpperCase()
 }
 
 // 14/04/20 (2.5)
 // timeframe is now stored in seconds
-if (STORED && STORED.timeframe > 1000) {
+if (STORED.timeframe > 1000) {
   STORED.timeframe /= 1000
 }
+// 22/04/20
+if (STORED.statsCounters) {
+  for (let counter of STORED.statsCounters) {
+    counter.output = counter.output.replace(/buyCount/g, 'cbuy').replace(/sellCount/g, 'csell').replace(/buyAmount/g, 'vbuy').replace(/sellAmount/g, 'vsell')
+  }
+}
 
-export default Object.assign({}, DEFAULTS, EXTRA, STORED || {}, QUERY_STRING);
+export default Object.assign({}, DEFAULTS, EXTRA, STORED, QUERY_STRING);
